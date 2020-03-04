de73898854b82bc8667952ba51e8319023dae6a4
shaun.riceshower <shaun.riceshower@gmail.com>
2020-03-04 13:30:13 +0000

    202003042230

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-    
+
     def index
         @tasks = Task.all
     end
@@ -43,6 +43,9 @@ class TasksController < ApplicationController
     end
 
     private
+        def set_task
+            @task = Task.find(params[:id])
+        end
         def task_params
             params.require(:task).permit(:content, :status)
         end
